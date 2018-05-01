4d63c976b7b0f9ed4e121f779c0f83690b0e82a4
ntzyr <vladislavsurikov@gmail.com>
2018-05-01 07:39:33 +0300

    Shortcodes: Get Posts
    @version 0.6
    
    Params
    posttype="[post|attachment|...]"
    count="[number]"
    title="[string]"

--- inc/classes/shortcode_templates/js/tinymce-get_posts.js
+++ inc/classes/shortcode_templates/js/tinymce-get_posts.js
@@ -4,22 +4,71 @@
         editor.addButton('get_posts', {
             title: 'Get Posts',
             cmd: 'get_posts',
-            image: url + '/get_posts.jpg',
+            image: url + '/icons/get_posts.png',
         });
  
         editor.addCommand('get_posts', function() {
-            var selected_text = editor.selection.getContent({
-                'format': 'html'
+            var win = editor.windowManager.open({
+                title: 'Get Posts Shortcode',
+                body: [
+                    {
+                        type: 'textbox',
+                        name: 'posttype',
+                        label: 'Post Type',
+                        minWidth: 500,
+                        value: ''
+                    },
+                    {
+                        type: 'textbox',
+                        name: 'count',
+                        label: 'Posts Count',
+                        minWidth: 500,
+                        value: ''
+                    },
+                    {
+                        type: 'textbox',
+                        name: 'title',
+                        label: 'Shortcode Title',
+                        minWidth: 500,
+                        value: ''
+                    }
+                ],
+                buttons: [
+                    {
+                        text: 'Submit',
+                        subtype: "primary",
+                        onclick: function() {
+                            win.submit();
+                        }
+                    },
+                    {
+                        text: 'Cancel',
+                        onclick: function() {
+                            win.close();
+                        }
+                    }
+                ],
+                onsubmit: function( e ) {
+                    var params = [];
+
+                    if( e.data.posttype.length > 0 ) {
+                        params.push('posttype="' + e.data.posttype + '"');
+                    }
+                    if( e.data.count.length > 0 ) {
+                        params.push('count="' + e.data.count + '"');
+                    }
+                    if( e.data.title.length > 0 ) {
+                        params.push('title="' + e.data.title + '"');
+                    }
+                    if( params.length > 0 ) {
+                        var paramsString = ' ' + params.join(' ');
+                        var return_text = '[get_posts' + paramsString + ']';
+                        editor.execCommand('mceInsertContent', false, return_text);
+                    }
+                }
             });
-            if ( selected_text.length === 0 ) {
-                alert( 'Please select some text.' );
-                return;
-            }
-            var open_column = '<div class="column">';
-            var close_column = '</div>';
-            var return_text = '';
-            return_text = open_column + selected_text + close_column;
-            editor.execCommand('mceReplaceContent', false, return_text);
+            // var shortcode = '<div class="column">';
+            // return_text = shortcode;
             return;
         });
  
