1c873ded675764d62274c245b11ef79d6f7eb3fa
ntzyr <vladislavsurikov@gmail.com>
2018-09-02 13:29:50 +0300

    Remove backend features (move to Codein Core)

--- functions.php
+++ functions.php
@@ -164,13 +164,3 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
-
-/**
- * Load CPT
- */
-require get_template_directory() . '/inc/custom_post_types.php';
-
-/**
- * Load CPT
- */
-require get_template_directory() . '/inc/custom_shortcodes.php';
--- inc/ajax.php
+++ inc/ajax.php
@@ -6,5 +6,3 @@ add_action( 'wp_enqueue_scripts', function () { // scripts for ajax pages
     $ajax_url = admin_url('admin-ajax.php');
     // wp_localize_script( 'script-name', 'ajax_url', $ajax_url );
 } );
-
-// require 'ajax/file.php';
